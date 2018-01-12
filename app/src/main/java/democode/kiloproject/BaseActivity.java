86e0360f74bb81c7eec2f86e610024fc58407722
kilo <kiloswodh@gmail.com>
2018-01-12 20:04:38 +0800

    v0.1

--- app/src/main/java/democode/kiloproject/BaseActivity.java
+++ app/src/main/java/democode/kiloproject/BaseActivity.java
@@ -1,8 +1,15 @@
 package democode.kiloproject;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import democode.kiloproject.widget.TitleView;
 
 /**
  * Created by Administrator on 2017/12/9.
@@ -15,5 +22,40 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mActivity = this;
+    }
+
+    protected void initTitle(TitleView titleView, int colorRes) {
+        boolean isImmersive = false;
+
+        if (colorRes == Color.WHITE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT > 19 && Build.VERSION.SDK_INT < 21) {
+            isImmersive = true;
+            //透明状态栏
+            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+//          getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        } else if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            isImmersive = true;
+        }
+        if (colorRes == Color.TRANSPARENT){
+            titleView.setImmersive(false);
+            titleView.setBackgroundColor(Color.TRANSPARENT);
+            this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }else {
+            titleView.setImmersive(isImmersive);
+            titleView.setBackgroundColor(getResources().getColor(colorRes));
+        }
     }
 }
