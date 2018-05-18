adec925bd61827855985c183d88983d277bee950
kilo <455565995@qq.com>
2018-05-18 21:42:59 +0800

    添加透明状态栏
    添加透明状态栏

--- app/src/main/java/democode/kiloproject/BaseActivity.java
+++ app/src/main/java/democode/kiloproject/BaseActivity.java
@@ -35,6 +35,28 @@ public abstract class BaseActivity extends AppCompatActivity {
     //对只需要一次初始化的耗时操作或者界面绘制放在这里
     abstract void initView();
 
+
+    //状态栏沉浸
+    protected void transparentStatusBar(boolean isWhiteBg) {
+        mImmersionBar = ImmersionBar.with(this);
+        if (isWhiteBg) {
+            mImmersionBar.statusBarDarkFont(true, 0.2f)
+                    //原理：如果当前设备支持状态栏字体变色，会设置状态栏字体为黑色，
+                    // 如果当前设备不支持状态栏字体变色，会使当前状态栏加上透明度，否则不执行透明度
+                    .transparentStatusBar()  //透明状态栏，不写默认透明色
+                    .transparentNavigationBar()
+                    .fitsSystemWindows(true)
+                    .keyboardEnable(true)  //解决软键盘与底部输入框冲突问题
+                    .init();
+        } else {
+            mImmersionBar.keyboardEnable(true)  //解决软键盘与底部输入框冲突问题
+                    .transparentStatusBar()  //透明状态栏，不写默认透明色
+                    .transparentNavigationBar()
+                    .fitsSystemWindows(true)
+                    .init();   //所有子类都将继承这些相同的属性
+        }
+    }
+
     //状态栏沉浸
     protected void initStateBar(int colorRes, boolean isWhiteBg) {
         mImmersionBar = ImmersionBar.with(this);
@@ -42,11 +64,15 @@ public abstract class BaseActivity extends AppCompatActivity {
             mImmersionBar.statusBarDarkFont(true, 0.2f)
                     //原理：如果当前设备支持状态栏字体变色，会设置状态栏字体为黑色，
                     // 如果当前设备不支持状态栏字体变色，会使当前状态栏加上透明度，否则不执行透明度
+                    .statusBarColor(colorRes)
+                    .navigationBarColor(colorRes)
                     .fitsSystemWindows(true)
                     .keyboardEnable(true)  //解决软键盘与底部输入框冲突问题
                     .init();
         } else {
             mImmersionBar.keyboardEnable(true)  //解决软键盘与底部输入框冲突问题
+                    .statusBarColor(colorRes)
+                    .navigationBarColor(colorRes)
                     .fitsSystemWindows(true)
                     .init();   //所有子类都将继承这些相同的属性
         }
